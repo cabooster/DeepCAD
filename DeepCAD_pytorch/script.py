d52b0d1ac2d59660aa2d62201ea057cb7bf846c3
cabooster <71710781+cabooster@users.noreply.github.com>
2020-12-31 10:33:57 +0800

    Update script.py

--- DeepCAD_pytorch/script.py
+++ DeepCAD_pytorch/script.py
@@ -1,10 +1,23 @@
 import os
 import time
+import sys
 
-# for train
-os.system('python train.py --datasets_folder DataForPytorch --lr 0.00005 \
-    --img_h 64 --img_w 64 --img_s 464 --gap_h 64 --gap_w 64 --gap_s 150 --n_epochs 20 --GPU 0 --normalize_factor 1 --train_datasets_size 1200 --select_img_num 10000')
+flag = sys.argv[1]
 
-# for test
-os.system('python test.py --denoise_model ModelForPytorch \
-    --datasets_folder DataForPytorch --datasets_path datasets --pth_path pth --output_dir results --test_datasize 2000')
+if flag == 'train':
+    # for train
+    os.system('python train.py --datasets_folder DataForPytorch --lr 0.00005 \
+        --img_h 64 --img_w 64 --img_s 464 --gap_h 64 --gap_w 64 --gap_s 150 --n_epochs 20 --GPU 0 --normalize_factor 1 --train_datasets_size 1200 --select_img_num 10000')
+
+if flag == 'test':
+    # for test
+    os.system('python test.py --denoise_model ModelForPytorch \
+        --datasets_folder DataForPytorch --datasets_path datasets --pth_path pth --output_dir results --test_datasize 2000')
+
+if flag == 'all':
+    # train and test
+    os.system('python train.py --datasets_folder DataForPytorch --lr 0.00005 \
+        --img_h 64 --img_w 64 --img_s 464 --gap_h 64 --gap_w 64 --gap_s 150 --n_epochs 20 --GPU 0 --normalize_factor 1 --train_datasets_size 1200 --select_img_num 10000')
+
+    os.system('python test.py --denoise_model ModelForPytorch \
+        --datasets_folder DataForPytorch --datasets_path datasets --pth_path pth --output_dir results --test_datasize 2000')
