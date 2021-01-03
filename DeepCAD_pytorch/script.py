1300b9e690239bc36084261179251ba1dfb4dcc2
cabooster <71710781+cabooster@users.noreply.github.com>
2021-01-03 13:16:03 +0800

    Update script.py

--- DeepCAD_pytorch/script.py
+++ DeepCAD_pytorch/script.py
@@ -12,7 +12,7 @@ if flag == 'train':
 if flag == 'test':
     # for test
     os.system('python test.py --denoise_model ModelForPytorch \
-        --datasets_folder DataForPytorch --datasets_path datasets --pth_path pth --output_dir results --test_datasize 2000')
+        --datasets_folder DataForPytorch --datasets_path datasets --pth_path pth --output_dir results --test_datasize 6000')
 
 if flag == 'all':
     # train and test
@@ -20,4 +20,4 @@ if flag == 'all':
         --img_h 64 --img_w 64 --img_s 464 --gap_h 64 --gap_w 64 --gap_s 150 --n_epochs 20 --GPU 0 --normalize_factor 1 --train_datasets_size 1200 --select_img_num 10000')
 
     os.system('python test.py --denoise_model ModelForPytorch \
-        --datasets_folder DataForPytorch --datasets_path datasets --pth_path pth --output_dir results --test_datasize 2000')
+        --datasets_folder DataForPytorch --datasets_path datasets --pth_path pth --output_dir results --test_datasize 6000')
